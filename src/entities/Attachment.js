e280f70fa2483226da3bc00bf8f7a0323e22a017
Tim van Osch <timvosch@pollexprojects.nl>
2018-11-08 13:05:35 +0100

    Update attachment propertymap

--- src/entities/Attachment.js
+++ src/entities/Attachment.js
@@ -2,13 +2,18 @@ import BusinessEntity from './BusinessEntity';
 
 export default class Attachment extends BusinessEntity {
 
+    static get PropertyMap() {
+        return {
+            updatedAt: 'Date'
+        };
+    }
+
     static GetEntityName() {
         return 'Attachment';
     }
 
     update(data) {
         if (!super.update(data)) return;
-        // Fill in
     }
 
 }
