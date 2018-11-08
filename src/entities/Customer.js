48742314552b71d812b7938f18ba3a6adcd417ca
Tim van Osch <timvosch@pollexprojects.nl>
2018-11-08 13:05:25 +0100

    Remove now unused property setting

--- src/entities/Customer.js
+++ src/entities/Customer.js
@@ -8,13 +8,6 @@ export default class Customer extends BusinessEntity {
 
     update(data) {
         if (!super.update(data)) return;
-        this.name = data.Name;
-        this.billingAddress = data.BillingAddress;
-        this.email = data.Email;
-        this.identifier = data.BusinessIdentifier;
-        // For each custom field create a CustomField entity
-        this.customFields = data.CustomFields;
-        this.code = data.Code;
     }
 
 }
