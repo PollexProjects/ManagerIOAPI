659ca7ee7688b3a7fa0e0082ab6d74140988aa18
Tim van Osch <timvosch@pollexprojects.nl>
2018-11-08 13:58:51 +0100

    Include a utility function

---
+++ src/utilities/SwapKV.js
@@ -0,0 +1,9 @@
+// I copied this even though it's very simple. I'll give the guy his credits :)
+// https://stackoverflow.com/questions/23013573/swap-key-with-value-json
+export function SwapKV(obj){
+    var ret = {};
+    for(var key in obj){
+        ret[obj[key]] = key;
+    }
+    return ret;
+}
