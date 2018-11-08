84e8b5cc5eb522a45c884bc5ddf7116f9f024ee1
Tim van Osch <timvosch@pollexprojects.nl>
2018-11-08 12:59:34 +0100

    Move entities rollup to one index file

--- src/entities/index.js
+++
@@ -1,9 +0,0 @@
-import ManagerEntity from 'ManagerEntity';
-import Business from './Business';
-import Customer from './Customer';
-
-export {
-    ManagerEntity,
-    Business,
-    Customer
-};
--- src/index.js
+++ src/index.js
@@ -1,11 +1,79 @@
+import Attachment from './entities/Attachment';
+import BalanceSheet from './entities/BalanceSheet';
+import BalanceSheetAccount from './entities/BalanceSheetAccount';
+import BalanceSheetControlAccount from './entities/BalanceSheetControlAccount';
+import BankAccount from './entities/BankAccount';
+import BankReconciliationBalance from './entities/BankReconciliationBalance';
+import BaseCurrency from './entities/BaseCurrency';
+import BillableTimeEntry from './entities/BillableTimeEntry';
+import Business from './entities/Business';
+import BusinessDetails from './entities/BusinessDetails';
+import CapitalSubaccount from './entities/CapitalSubaccount';
+import CashSummary from './entities/CashSummary';
+import Customer from './entities/Customer';
+import CustomField from './entities/CustomField';
+import CustomTaxReport from './entities/CustomTaxReport';
+import Email from './entities/Email';
+import EmailTemplate from './entities/EmailTemplate';
+import InBuiltTaxCode from './entities/InBuiltTaxCode';
+import InterAccountTransfer from './entities/InterAccountTransfer';
+import JournalEntry from './entities/JournalEntry';
+import Logo from './entities/Logo';
 import ManagerBroker from './ManagerBroker';
 import ManagerEntity from './entities/ManagerEntity';
-import Business from './entities/Business';
-import Customer from './entities/Customer';
+import ProfitAndLossStatement from './entities/ProfitAndLossStatement';
+import ProfitAndLossStatementAccount from './entities/ProfitAndLossStatementAccount';
+import ProfitAndLossStatementControlAccount from './entities/ProfitAndLossStatementControlAccount';
+import ProfitAndLossStatementGroup from './entities/ProfitAndLossStatementGroup';
+import PurchaseInvoice from './entities/PurchaseInvoice';
+import ReceiptOrPayment from './entities/ReceiptOrPayment';
+import RecurringPurchaseInvoice from './entities/RecurringPurchaseInvoice';
+import RecurringSalesInvoice from './entities/RecurringSalesInvoice';
+import SalesInvoice from './entities/SalesInvoice';
+import StartDate from './entities/StartDate';
+import StatementOfChangesInEquity from './entities/StatementOfChangesInEquity';
+import SummaryStartDate from './entities/SummaryStartDate';
+import Supplier from './entities/Supplier';
+import Tabs from './entities/Tabs';
+import TaxSummary from './entities/TaxSummary';
 
 export {
+    Attachment,
+    BalanceSheet,
+    BalanceSheetAccount,
+    BalanceSheetControlAccount,
+    BankAccount,
+    BankReconciliationBalance,
+    BaseCurrency,
+    BillableTimeEntry,
+    Business,
+    BusinessDetails,
+    CapitalSubaccount,
+    CashSummary,
+    Customer,
+    CustomField,
+    CustomTaxReport,
+    Email,
+    EmailTemplate,
+    InBuiltTaxCode,
+    InterAccountTransfer,
+    JournalEntry,
+    Logo,
     ManagerBroker,
     ManagerEntity,
-    Business,
-    Customer
+    ProfitAndLossStatement,
+    ProfitAndLossStatementAccount,
+    ProfitAndLossStatementControlAccount,
+    ProfitAndLossStatementGroup,
+    PurchaseInvoice,
+    ReceiptOrPayment,
+    RecurringPurchaseInvoice,
+    RecurringSalesInvoice,
+    SalesInvoice,
+    StartDate,
+    StatementOfChangesInEquity,
+    SummaryStartDate,
+    Supplier,
+    Tabs,
+    TaxSummary
 };
