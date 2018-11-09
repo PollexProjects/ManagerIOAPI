5b565ed95926e8beec070f46f884f254188b4e2f
Tim van Osch <timvosch@pollexprojects.nl>
2018-11-09 07:48:33 +0100

    Make default export

--- src/utilities/SwapKV.js
+++ src/utilities/SwapKV.js
@@ -1,6 +1,6 @@
 // I copied this even though it's very simple. I'll give the guy his credits :)
 // https://stackoverflow.com/questions/23013573/swap-key-with-value-json
-export function SwapKV(obj){
+export default function SwapKV(obj){
     var ret = {};
     for(var key in obj){
         ret[obj[key]] = key;
